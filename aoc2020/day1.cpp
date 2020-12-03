7787a6c9a5f1bea6e5176ea34b54edf601fb36b7
Corey Grief <cogrief@microsoft.com>
2020-12-02 17:00:43 -0800

    optimized vector conversion

--- aoc2020/day1.cpp
+++ aoc2020/day1.cpp
@@ -1,24 +1,28 @@
 #include "day1.h"
 #include <iostream>;
 #include <unordered_map>;
+#include <algorithm>
 
 
 int day1::run(vector<string> input, bool isPartB) {
+
+	std::vector<int> input_int;
+	std::transform(input.begin(), input.end(), std::back_inserter(input_int), [](const string& str) { return std::stoi(str); });
+
 	if (isPartB) {
-		return partB(input);
+		return partB(input_int);
 	}
 	else {
-		return partA(input);
+		return partA(input_int);
 	}
 }
 
-int day1::partA(vector<string> input) {
+int day1::partA(vector<int> input) {
 	
 	bool numMap[2020] {};
 
-	for (string item : input) {
-		int val = std::stoi(item);
-		
+	for (int val : input) {
+
 		if (numMap[2020 - val]) {
 			return val * (2020 - val);
 		}
@@ -30,14 +34,14 @@ int day1::partA(vector<string> input) {
 	return -1;
 }
 
-int day1::partB(vector<string> input) {
+int day1::partB(vector<int> input) {
 
 	bool numMap[2020]{};
 
 	for (int i = 0; i < input.size(); i++) {
-		int val_i = std::stoi(input[i]);
+		int val_i = input[i];
 		for (int j = i + 1; j < input.size(); j++) {
-			int val_j = std::stoi(input[j]);
+			int val_j = input[j];
 			if ((2020-val_i-val_j > 0) && (numMap[2020 - val_i - val_j])) {
 				return val_i * val_j * (2020 - val_i - val_j);
 			}
--- aoc2020/day1.h
+++ aoc2020/day1.h
@@ -9,7 +9,7 @@ class day1
 {
 	public:
 		static int run(vector<string> input, bool partB);
-		static int partA(vector<string> input);
-		static int partB(vector<string> input);
+		static int partA(vector<int> input);
+		static int partB(vector<int> input);
 };
 
